d5731d01f4cc82eabad47709dd7ab2b206521024
Gaëtan HERFRAY <gaetan.hfy@gmail.com>
2017-04-27 08:36:51 +0200

    Add random second question

--- app/src/main/java/net/gahfy/samplequizz/QuizzPresenter.java
+++ app/src/main/java/net/gahfy/samplequizz/QuizzPresenter.java
@@ -1,21 +1,16 @@
 package net.gahfy.samplequizz;
 
 import java.util.List;
+import java.util.Random;
 
 public class QuizzPresenter {
     private QuizzView mView;
-    private Question mQuestion = new Question(
-            "En quelle année est sortie le premier téléphone sous Android ?",
-            "2008",
-            new String[]{
-                    "2006",
-                    "2007",
-                    "2009"
-            }
-            );
+    private Question mQuestion;
 
     QuizzPresenter(QuizzView view){
         mView = view;
+
+        initQuestion();
         presentQuestion();
     }
 
@@ -25,6 +20,31 @@ public class QuizzPresenter {
 
     public Question getQuestion(){
         return mQuestion;
+    }
+
+    private void initQuestion(){
+        Question[] questions = new Question[]{
+                new Question(
+                        "En quelle année est sortie le premier téléphone sous Android ?",
+                        "2008",
+                        new String[]{
+                                "2006",
+                                "2007",
+                                "2009"
+                        }
+                ),
+                new Question(
+                        "Quelle société développe le système d'exploitation Android ?",
+                        "Google",
+                        new String[]{
+                                "Microsoft",
+                                "IBM",
+                                "Apple"
+                        }
+                )
+        };
+        int randId = new Random().nextInt(questions.length);
+        mQuestion = questions[randId];
     }
 
     private void presentQuestion(){
